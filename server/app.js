2756540560b6d0bb502d6ba53501af97dd4ec432
Andrew Furth <andrew.furth@gmail.com>
2016-06-30 15:44:24 -0700

    Show route functional

--- server/app.js
+++ server/app.js
@@ -17,17 +17,38 @@ var bookSchema = new mongoose.Schema({
 
 mongoose.model('books', bookSchema)
 
+// INDEX
 app.get('/books', (req, res) => {
   mongoose.model('books').find(function(err, books) {
     res.render('index', {books: books});
   })
 })
 
+// NEW
 app.get('/books/new', (req, res) => {
   res.render('new')
 })
 
+// SHOW
+app.get('/books/:id', (req, res) => {
+  mongoose.model('books').findById(req.params.id, function(err, book) {
+    if (err) {
+      console.log(err)
+      res.redirect('/books')
+    }
+    else {
+      res.render('show', {book:book})
+    }
+  })
+})
 
+
+// EDIT
+app.get('/books/:id/edit', (req, res) => {
+  
+})
+
+// CREATE
 app.post('/books', (req, res) => {
   mongoose.model('books').create({title: req.body.title, author: req.body.author}, function(err, book) {
     if (err) {
@@ -41,6 +62,13 @@ app.post('/books', (req, res) => {
   })
 })
 
+// UPDATE
+
+
+// DELETE
+
+
+// CATCH-ALL
 app.get('*', (req, res) => {
   res.redirect('/books')
 })
